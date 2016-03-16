06c1388a5575323e8871388230c6e5494e84c1bd
Robin <iluvatar.robin@web.de>
2016-03-16 13:10:54 +0100

    very basic app skelleton

--- App/Archase/app/src/main/java/com/snigelentertainments/snigelgroup/archase/MainActivity.java
+++ App/Archase/app/src/main/java/com/snigelentertainments/snigelgroup/archase/MainActivity.java
@@ -1,10 +1,12 @@
 package com.snigelentertainments.snigelgroup.archase;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.snigelentertainments.snigelgroup.archase.planechase.PlanechaseLaunch;
 
@@ -26,8 +28,15 @@ public class MainActivity extends AppCompatActivity {
     public void onClickPlanechase(View view){
         Log.d(TAG, "launching planechase laucnher");
         if (view.getId() == R.id.b_planechase){
-            Intent i = new Intent(MainActivity.this, PlanechaseLaunch.class);
-            startActivity(i);
+            Context context = this.getApplicationContext();
+            String text = "Planechase not yet implemented!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            //Intent i = new Intent(MainActivity.this, PlanechaseLaunch.class);
+            //startActivity(i);
 
         }
         else {
@@ -39,12 +48,33 @@ public class MainActivity extends AppCompatActivity {
     public void onClickArchenemy(View view){
         Log.d(TAG, "launching archenemy launcher");
         if (view.getId() == R.id.b_archenemy){
+            Context context = this.getApplicationContext();
+            String text = "Archenemy not yet implemented!";
+            int duration = Toast.LENGTH_SHORT;
 
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
         else {
             String v = (String)(view == null?"null":view.getId());
             Log.w(TAG, String.format("Tried launching archenmy with the wrong view: %s", v));
         }
 
+    }
+
+    public void onClickImpressum(View view){
+        Log.d(TAG, "launching Impressumg");
+        if (view.getId() == R.id.b_impressum){
+            Context context = this.getApplicationContext();
+            String text = "Impressum not yet implemented!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else {
+            String v = (String)(view == null?"null":view.getId());
+            Log.w(TAG, String.format("Tried launching archenmy with the wrong view: %s", v));
+        }
     }
 }
